784f63d793b5301ef1228f26c5e6c151f877c417
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-28 21:58:13 +0300

    init: start dream-team task

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,6 +2,6 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(array) {
   let newArray = array.flat(Infinity),
-  howMuchCats = String(newArray).split('^^').length-1;
-  return howMuchCats;
+  howManyCats = String(newArray).split('^^').length-1;
+  return howManyCats;
 };
