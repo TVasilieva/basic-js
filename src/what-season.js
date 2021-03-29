3ff79fccb61d215b3735e7b864edb7a0f9c4d3b2
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-29 21:13:04 +0300

    feat: fix month for winter

--- src/what-season.js
+++ src/what-season.js
@@ -1,10 +1,11 @@
 module.exports = function getSeason(date) {
+  if(!(Object.prototype.toString.call(date) === "[object Date]")) throw new Error()
   if(typeof(date) == 'undefined') return 'Unable to determine the time of year!'
 
   let month = date.getMonth();
-  if(month < 1 || month == 11) return 'winter';
-  else if(month > 2 && month < 5) return 'spring';
-  else if(month > 5 && month < 8) return 'summer';
+  if(month < 2 || month == 11) return 'winter';
+  else if(month < 5) return 'spring';
+  else if(month < 8) return 'summer';
   else return 'autumn';
 
 };
