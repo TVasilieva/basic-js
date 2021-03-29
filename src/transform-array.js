0c9c307d27f04a53c774152510a3e599052ed71f
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-29 22:08:42 +0300

    feat: change from for-of to for with [i]; j k k k k npm test git status

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,3 +1,17 @@
 module.exports = function transform(arr) {
-  
+  if (!Array.isArray(arr)) throw new Error();
+  let array =  [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == '--discard-next'){
+      i++;
+      };
+    }else if(arr[i] == '--discard-prev'){
+
+    }else if(arr[i] == '--double-next'){
+
+    }else if(arr[i] == '--double-prev'){
+
+    }else{
+
+    }
 };
