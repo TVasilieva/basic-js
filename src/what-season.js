ed289c12651ff44da0eb1f59f45cf9cce71035e7
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-29 21:08:58 +0300

    feat: fix problems with a range of month

--- src/what-season.js
+++ src/what-season.js
@@ -1,3 +1,10 @@
-module.exports = function getSeason(/* date */) {
- 
+module.exports = function getSeason(date) {
+  if(typeof(date) == 'undefined') return 'Unable to determine the time of year!'
+
+  let month = date.getMonth();
+  if(month < 1 || month == 11) return 'winter';
+  else if(month > 2 && month < 5) return 'spring';
+  else if(month > 5 && month < 8) return 'summer';
+  else return 'autumn';
+
 };
