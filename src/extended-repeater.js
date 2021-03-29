cbe048e725450b483c029892da2fc0c9af2bca85
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-29 23:45:56 +0300

    feat: add a solution for =repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+let str = String(str);
+
+ let repeatTimes = options.repeatTimes, separator, addition, additionRepeatTimes = options.additionRepeatTimes, additionSeparator;
+
+  if (repeatTimes == null) {
+      separator = "";
+  } else if (options.separator == null) {
+      separator = "+";
+  } else {
+      separator = options.separator;
+  }
+
+  if (String(options.addition) === "null") {
+      addition = "null";
+  } else if (String(options.addition) === "undefined") {
+      addition = "";
+  } else {
+      addition = String(options.addition);
+  }
+
+  let add = addition;
+
+  if (options.additionSeparator == null) {
+      additionSeparator = "|";
+  } else {
+    additionSeparator = options.additionSeparator;
+  }
+  
+  for (let i = 0; i < additionRepeatTimes - 1; i++) {
+    add = add + additionSeparator + addition;
+  }
+
+  let newStr = str + add;
+  if (additionRepeatTimes == null) {
+      add = "";
+  }
+  for(let i = 0; i < repeatTimes - 1; i++) {
+      newStr = newStr + separator + str + add;
+  }
+  if(repeatTimes == null) {
+      newStr += add;
+  }
+  return newStr;
 };
   
