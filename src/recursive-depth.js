e79023675a04131563a633ed8ce560891ed899e0
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-29 22:34:39 +0300

    feat: add a decision

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,5 +1,16 @@
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-
+    let depth = 1, currentDepth = 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        currentDepth += this.calculateDepth(arr[i]);
+      }
+      if (currentDepth > depth) {
+        depth = currentDepth;
+      } 
+      currentDepth = 1;
+    }
+    let result = depth, currentDepth = 1;
+    return result;
   }
-};
+}
