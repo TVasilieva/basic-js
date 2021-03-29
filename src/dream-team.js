3480deb7133e759d1eb3c7c55c15602bba01f329
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-29 19:52:06 +0300

    feat: fix the problem with array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  
+  let highLetters = '';
+    for(const x of members){
+      if(x === String(x)){
+        let value = x.trim();
+        if(value == (value.charAt(0).toUpperCase() + value.slice(1))){
+          highLetters = highLetters + value.charAt(0);
+        }
+      } else if(value !== String(value)) {
+    }
+  }
+  return highLetters.split('').sort().join('');
 };
